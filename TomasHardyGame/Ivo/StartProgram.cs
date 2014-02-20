9a5cea16a47591cfadfb8412a3781b50c869344a
ivossss <ivossss@yahoo.com>
2014-02-20 21:41:32 +0200

    My idea for level handling ver 1.0

---
+++ TomasHardyGame/Ivo/StartProgram.cs
@@ -0,0 +1,21 @@
+﻿namespace Ivo
+{
+    class StartProgram
+    {
+        static void Main()
+        {
+            string filePath = "../../Map level 1.txt";
+            var mapHandling = new MapHandling(filePath);
+            mapHandling.ReadFromFile();
+            mapHandling.LoadMapOnScreen();
+
+            var player = new PlayerMovement(mapHandling.MapMatrix);
+            player.GetPlayerStartPosition();
+
+            while (true)
+            {
+                player.Move();
+            }
+        }
+    }
+}
