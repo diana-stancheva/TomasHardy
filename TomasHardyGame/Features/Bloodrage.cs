aeb4f1b3a37ab997344233a7e5aef2bf3b261f3d
Diana Stancheva <diana@calderra.com>
2014-02-21 11:39:23 +0200

    added extension class for magic

--- TomasHardyGame/Features/Bloodrage.cs
+++ TomasHardyGame/Features/Bloodrage.cs
@@ -6,21 +6,20 @@ using System.Threading.Tasks;
 
 namespace Features
 {
-    class Bloodrage : Magic, IEnchantable
+    public class Bloodrage : Magic, IEnchantable
     {
         public Bloodrage()
             :base()
         {
             this.Name = "Blood Rage";
             this.Description = "Drives a unit into a bloodthirsty rage, during which it has higher attack damage, but cannot cast spells and takes damage every second";
-            //this.Hero = 
             this.ManaCost = 80;
             this.CooldownTime = 12;
         }
 
         public void Use()
         {
-            //
+            throw new NotImplementedException();
         }
     }
 }
--- TomasHardyGame/Features/Magic.cs
+++ TomasHardyGame/Features/Magic.cs
@@ -2,7 +2,7 @@
 {
     public abstract class Magic
     {
-        public string Name { get; protected set; }
+        public string Name { get;  protected set; }
         public string Description { get; protected set; }
         public string Hero { get; protected set; }
         public int ManaCost { get; protected set; }
