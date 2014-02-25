5c4d2483168184c1c0b0635b21fa17218ccafcba
Diana Stancheva <diana@calderra.com>
2014-02-25 14:24:41 +0200

    class diagram

---
+++ TomasHardyGame/Dota/Features/Feature.cs
@@ -0,0 +1,14 @@
+﻿namespace Dota
+{
+    public abstract class Feature
+    {
+        public string Name { get; protected set; }
+        public string Description { get; protected set; }
+        //public string Hero { get; protected set; }
+
+        public Feature()
+        {
+
+        }
+    }
+}
